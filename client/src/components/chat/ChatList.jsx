f57c4c71131ad12633bcaf139c870ac48dda1bb1
Nicohena <nicohena19@gmail.com>
2026-02-24 01:17:14 +0300

    refactored chat

--- client/src/components/chat/ChatList.jsx
+++ client/src/components/chat/ChatList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import chatService from "../../api/chatService";
 import socket from "../../utils/socket";
+import { getImageUrl } from "../../utils/imageUtils";
 
 const ChatList = ({
   onSelectConversation,
